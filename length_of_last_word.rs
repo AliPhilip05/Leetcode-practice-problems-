0f327b469b078bdbf6f4de3f8a80dd45d4420446
Ali Philip <131696518+AliPhilip05@users.noreply.github.com>
2026-09-22 22:03:07 -0400

    Add function to find length of last word
    Implement function to calculate length of last word in a string.

---
+++ length_of_last_word.rs
@@ -0,0 +1,51 @@
+/*
+58. Length of Last Word
+Solved
+Easy
+
+Topics
+conpanies icon
+Companies
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+There will be at least one word in s. */
+
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+       let mut v: Vec<i32> = Vec::new();
+        for word in s.split_whitespace().rev() {
+            let length: i32 = word.len() as i32;
+            v.push(length);
+
+            println!("{length}");
+        }
+
+        let length = v[0];
+        length
+    }
+}
